aeae374f24a299f7ce36cb6e11c5b9c35adf5ed5
peppermintcoding <58445479+peppermintcoding@users.noreply.github.com>
2022-11-30 09:49:57 +0000

    create dictionary from txt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::fs;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+
+
+fn build_dictionary(file_path: &str) -> HashMap<String, HashMap<String, u32>> {
+    let mut dictionary: HashMap<String, HashMap<String, u32>> = HashMap::new();
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    let mut previous = "";
+    for word in contents.split_whitespace() {
+        let values = match dictionary.entry(previous.to_string()) {
+            Entry::Occupied(o) => o.into_mut(),
+            Entry::Vacant(v) => v.insert(HashMap::new()),
+        };
+        *values.entry(word.to_owned()).or_insert(1) += 1;
+        previous = word;
+    }
+
+    return dictionary;
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let dictionary = build_dictionary("alice.txt");
+
+    println!("{:?}", dictionary);
 }
