9b7446444a1994d158c2f373a71d085334902e7f
peppermintcoding <58445479+peppermintcoding@users.noreply.github.com>
2022-11-30 10:23:49 +0000

    working example

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ A simple Markov Chain in Rust using the *Alice in Wonderland* Book.
 #### Installation for Codespaces
 
  - `curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`
+
+
+#### Usage
+
+ - `cargo run [SEED WORD] [NUM ITERATIONS]`
+ - `cargo run Alice 50`
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::fs;
+use std::env;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
+
+use rand::Rng;
 
 
 fn build_dictionary(file_path: &str) -> HashMap<String, HashMap<String, u32>> {
@@ -23,7 +26,36 @@ fn build_dictionary(file_path: &str) -> HashMap<String, HashMap<String, u32>> {
 
 
 fn main() {
-    let dictionary = build_dictionary("alice.txt");
+    let mut dictionary = build_dictionary("alice.txt");
+    let args: Vec<String> = env::args().collect();
+    let mut seed = args[1].to_owned();
+    let iterations = args[2].parse::<u32>().unwrap_or(10);
 
-    println!("{:?}", dictionary);
+    let mut rng = rand::thread_rng();
+
+    print!("{} ", seed);
+
+    for _ in 1..=iterations {
+        let values = match dictionary.entry(seed.clone()) {
+            Entry::Occupied(o) => o.into_mut(),
+            Entry::Vacant(_) => break,
+        };
+        
+        let mut max_value = 0;
+        for (_, value) in values.iter() {
+            max_value += value;
+        }
+
+        let mut choice: i64 = rng.gen_range(0..=max_value).try_into().unwrap();
+        for (key, value) in values.iter() {
+            choice -= *value as i64;
+            if choice <= 0 {
+                print!("{} ", key);
+                seed = String::from(key);
+                break;
+            }
+        }
+    }
+    println!();
+
 }
